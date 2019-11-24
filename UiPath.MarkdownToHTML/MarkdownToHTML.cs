9239fd9f7ca669691f8d9998a18b9512b1005a2c
Varun <varun@skcript.com>
2019-11-24 11:52:46 -0800

    Initial Commit

---
+++ UiPath.MarkdownToHTML/MarkdownToHTML.cs
@@ -0,0 +1,29 @@
+﻿using Markdig;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UiPath.MarkdownToHTML
+{
+    public class MarkdownToHTML : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> InputMarkdownString { get; set; }
+
+        [Category("Output")]
+        [RequiredArgument]
+        public OutArgument<string> OutputHTMLString { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var inputMarkdown = InputMarkdownString.Get(context);
+            string result = Markdown.ToHtml(inputMarkdown);
+            OutputHTMLString.Set(context, result);
+        }
+    }
+}
